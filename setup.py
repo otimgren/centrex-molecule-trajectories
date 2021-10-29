5e0df6e311ee6f517c66fe00e6e12619f23844f6
otimgren <34794187+otimgren@users.noreply.github.com>
2021-10-29 18:39:08 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1' 
+DESCRIPTION = 'Python package for simulating molecular trajectories in CeNTREX'
+
+# Setting up
+setup(
+        name="trajectories",
+        version=VERSION,
+        author="Oskari Timgren",
+        author_email="<oskari.timgren@gmail.com>",
+        description=DESCRIPTION,
+        packages=find_packages(where = 'src'),
+        package_dir = {'':"src"},
+        install_requires=['numpy', 'scipy', 'joblib'],    
+        keywords=['python', ],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Programming Language :: Python :: 3",
+            "Operating System :: OS Independent",
+        ]
+)
